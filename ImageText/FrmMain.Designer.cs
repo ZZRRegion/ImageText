9d83db6ee79539164f566bcf475425868c0a2cf3
ZZRRegion <854974242@qq.com>
2018-12-21 21:32:12 +0800

    在图片上写文字

---
+++ ImageText/FrmMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ImageText
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnAddText = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnMany = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(44, 421);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择图片";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // pic
+            // 
+            this.pic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic.Location = new System.Drawing.Point(44, 15);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(400, 400);
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(44, 465);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(319, 21);
+            this.txtContent.TabIndex = 2;
+            this.txtContent.Text = "sfsdfsf";
+            // 
+            // btnAddText
+            // 
+            this.btnAddText.Location = new System.Drawing.Point(369, 464);
+            this.btnAddText.Name = "btnAddText";
+            this.btnAddText.Size = new System.Drawing.Size(75, 23);
+            this.btnAddText.TabIndex = 3;
+            this.btnAddText.Text = "加文字";
+            this.btnAddText.UseVisualStyleBackColor = true;
+            this.btnAddText.Click += new System.EventHandler(this.btnAddText_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(348, 421);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(96, 23);
+            this.btnSaveAs.TabIndex = 4;
+            this.btnSaveAs.Text = "图片另存为...";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnMany
+            // 
+            this.btnMany.Location = new System.Drawing.Point(185, 421);
+            this.btnMany.Name = "btnMany";
+            this.btnMany.Size = new System.Drawing.Size(75, 23);
+            this.btnMany.TabIndex = 5;
+            this.btnMany.Text = "批量";
+            this.btnMany.UseVisualStyleBackColor = true;
+            this.btnMany.Click += new System.EventHandler(this.btnMany_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 493);
+            this.Controls.Add(this.btnMany);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnAddText);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.btnSelect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "图片加文字";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnAddText;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnMany;
+    }
+}
+
---
+++ ImageText/FrmMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using System.IO;
+using System.Diagnostics;
+
+namespace ImageText
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "picture|*.png;*.bmp;*.jpg;";
+            if(ofd.ShowDialog(this) == DialogResult.OK)
+            {
+                this.pic.BackgroundImage = new Bitmap(ofd.FileName);
+                this.pic.Tag = ofd.FileName;
+            }
+        }
+
+        private void btnAddText_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> draw = new Image<Bgr, byte>(this.pic.Tag as string);
+            draw.Draw(this.txtContent.Text, new Point(100, 100), FontFace.HersheyDuplex, 5, new Bgr(0, 255, 255), 10);
+            this.pic.BackgroundImage = draw.ToBitmap();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            string fileName = @"tim55g.jpg";
+            this.pic.BackgroundImage = new Bitmap(fileName);
+            this.pic.Tag = fileName;
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "picture|*.jpg;*.bmp;*.png";
+            if(sfd.ShowDialog(this) == DialogResult.OK)
+            {
+                this.pic.BackgroundImage.Save(sfd.FileName);
+            }
+        }
+
+        private void btnMany_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                string path = @"D:\BaiduNetdiskDownload\动漫";
+                string savePath = Path.Combine(path, "many");
+                if (!Directory.Exists(savePath))
+                {
+                    Directory.CreateDirectory(savePath);
+                }
+                foreach (string fileName in Directory.GetFiles(path))
+                {
+                    Image<Bgr, byte> img = new Image<Bgr, byte>(fileName);
+                    img.Draw(this.txtContent.Text, new Point(100, 100), FontFace.HersheyDuplex, 5, new Bgr(0, 255, 255), 10);
+                    string name = Path.GetFileName(fileName);
+                    string saveAsFileName = Path.Combine(savePath, name);
+                    img.Save(saveAsFileName);
+                    img.Dispose();
+                }
+                stopwatch.Stop();
+                Action action = () => {
+                    this.Text = stopwatch.Elapsed.ToString();
+                };
+                this.Invoke(action);
+            });
+           
+        }
+    }
+}
